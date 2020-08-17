0a7ef4bbe4dab0c6b974d2b342f4d8ffc9e2fd47
JakeFrostyYT <lolcraftpvp32@gmail.com>
2020-08-17 12:55:11 +0800

    Version 62

---
+++ script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Placeholder for now
+result=Success
+if [ "$result" == "Success" ]; then
+	echo "[+] Update script Finished successfully!"
+else
+	echo "[-] Update script failed somehow?"
+fi
---
+++ update.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+if [[ $EUID -ne 0 ]]; then
+	echo "Please run this as root"
+	exit 1
+fi
+echo "[+] Starting Update Script..."
+echo "[+] Chroot version $buildver"
+wget -P /etc/update/ https://raw.githubusercontent.com/JakeFrostyYT/ChrootUpdateScript/master/version.txt
+newver=$(cat "/etc/update/version.txt")
+if [ $buildver \> $newver ]; then
+	echo "[+] Already on latest update"
+	exit 1
+else
+	echo "[+] New Version $newver available"
+	echo "Proceed with update? y/n"
+	read input
+	if [ "$input" == "y" ]; then
+		echo "[+] Updating..."
+		git clone https://github.com/JakeFrostyYT/ChrootUpdateScript.git "/etc/update/ChrootUpdateScript"
+		chmod 755 /etc/update/ChrootUpdateScript/script.sh
+		/etc/update/ChrootUpdateScript/script.sh
+	fi
+fi
