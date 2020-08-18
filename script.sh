6aab1c0fcd6202bebed2eadbd7a4f9dcfc4d4839
JakeFrostyYT <lolcraftpvp32@gmail.com>
2020-08-18 18:45:03 +0800

    Version 67

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@
 # Placeholder for now
 result=Success
 if [ "$result" == "Success" ]; then
-	echo "[+] Update script Finished successfully!"
+	echo "[+] Update script Finished successfully! Test update placeholder for version 67"
 else
-	echo "[-] Update script failed somehow?"
+	echo "[-] Update script failed somehow? Test update placeholder for version 67"
 fi
--- update.sh
+++ update.sh
@@ -1,39 +1,38 @@
 #!/bin/bash
+
+# Check for root
 if [[ $EUID -ne 0 ]]; then
 	echo "Please run this as root"
 	exit 1
 fi
 echo "[+] Starting Update Script..."
 echo "[+] Chroot version $buildver"
+
+# Get current version via github repository
 wget -P /etc/update/ https://raw.githubusercontent.com/JakeFrostyYT/ChrootUpdateScript/master/version.txt -O version.txt
 newver=$(cat "/etc/update/version.txt")
+
+# Compare current build version of chroot to newest version
+# Clone if newest version found and execute the script
 if [ "$buildver" -eq "$newver" ]; then
 	echo "[+] Already on latest update"
 	exit 1
 else
-<<<<<<< HEAD
-	if [ "$newver" -gt "$buildver" ]; then
+	if [ "$buildver" -eq "$newver" ]; then
+		echo "[+] Already on latest update"
+		exit 1
+	else
 		echo "[+] New Version $newver available"
 		echo "Proceed with update? y/n"
 		read input
 		if [ "$input" == "y" ]; then
 			echo "[+] Updating..."
 			git clone https://github.com/JakeFrostyYT/ChrootUpdateScript.git "/etc/update/ChrootUpdateScript"
+			rm /etc/update/ChrootUpdateScript/version.txt
 			chmod 755 /etc/update/ChrootUpdateScript/script.sh
 			/etc/update/ChrootUpdateScript/script.sh
+			rm /root/version
+			mv /etc/update/version.txt /root/version
 		fi
-=======
-	echo "[+] New Version $newver available"
-	echo "Proceed with update? y/n"
-	read input
-	if [ "$input" == "y" ]; then
-		echo "[+] Updating..."
-		git clone https://github.com/JakeFrostyYT/ChrootUpdateScript.git "/etc/update/ChrootUpdateScript"
-		rm /etc/update/ChrootUpdateScript/version.txt
-		chmod 755 /etc/update/ChrootUpdateScript/script.sh
-		/etc/update/ChrootUpdateScript/script.sh
-		rm /root/version
-		mv /etc/update/version.txt /root/version
->>>>>>> 5c6933027c9fc810fd588dbd13ef5d23365afdfb
 	fi
 fi
