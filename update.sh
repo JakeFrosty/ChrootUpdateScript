ce0a08c56b7f8f59c3620ebc33c2bd0e8307aea7
JakeFrostyYT <lolcraftpvp32@gmail.com>
2020-08-18 18:18:16 +0800

    Version 67

--- update.sh
+++ update.sh
@@ -7,17 +7,19 @@ echo "[+] Starting Update Script..."
 echo "[+] Chroot version $buildver"
 wget -P /etc/update/ https://raw.githubusercontent.com/JakeFrostyYT/ChrootUpdateScript/master/version.txt -O version.txt
 newver=$(cat "/etc/update/version.txt")
-if [ $buildver \> $newver ]; then
+if [ "$buildver" -eq "$newver" ]; then
 	echo "[+] Already on latest update"
 	exit 1
 else
-	echo "[+] New Version $newver available"
-	echo "Proceed with update? y/n"
-	read input
-	if [ "$input" == "y" ]; then
-		echo "[+] Updating..."
-		git clone https://github.com/JakeFrostyYT/ChrootUpdateScript.git "/etc/update/ChrootUpdateScript"
-		chmod 755 /etc/update/ChrootUpdateScript/script.sh
-		/etc/update/ChrootUpdateScript/script.sh
+	if [ "$newver" -gt "$buildver" ]; then
+		echo "[+] New Version $newver available"
+		echo "Proceed with update? y/n"
+		read input
+		if [ "$input" == "y" ]; then
+			echo "[+] Updating..."
+			git clone https://github.com/JakeFrostyYT/ChrootUpdateScript.git "/etc/update/ChrootUpdateScript"
+			chmod 755 /etc/update/ChrootUpdateScript/script.sh
+			/etc/update/ChrootUpdateScript/script.sh
+		fi
 	fi
 fi
