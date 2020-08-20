906d0883d4296379bc86ed32a57e2c077558fa5e
JakeFrostyYT <lolcraftpvp32@gmail.com>
2020-08-20 17:15:25 +0800

    Build 76

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ChrootUpdateScript
+## Currently V2 Build 76
 This will be cloned by my chroot, this script will be used to push updates to my chroot to tweak without needing to redownload the whole chroot
--- script.sh
+++ script.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 # Placeholder for now
-result=Success
+# Build 76 minor patch to chroot
+rm /usr/share/mitmf/plugins/filepwn.py
 if [ "$result" == "Success" ]; then
-	echo "[+] Update script Finished successfully! Test update placeholder for version 75"
+	echo "[+] Update script Finished successfully!"
 	# DO NOT MODIFY
 	#wget -P /usr/bin/ https://raw.githubusercontent.com/JakeFrostyYT/ChrootUpdateScript/master/chrootupdate
 	#chmod 755 /usr/bin/chrootupdate
 else
-	echo "[-] Update script failed somehow? Test update placeholder for version 75"
+	echo "[-] Update script failed somehow?"
 fi
